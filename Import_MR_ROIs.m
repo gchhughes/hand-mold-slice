8d610d00da2c2ce4d82c5ed84cbc60cc9fb511e9
gchhughes <74072434+gchhughes@users.noreply.github.com>
2022-01-12 19:00:17 -0800

    Added Jake's interpolation/labeling code

--- Import_MR_ROIs.m
+++ Import_MR_ROIs.m
@@ -3,14 +3,15 @@
 %   CASIT                                                                %
 %   Author: Griffith Hughes                                              %
 %   Date:   01/12/2022                                                   %
+%   Parse through .ROI file to create trace masks                        %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% function [mask,data,key] = Import_MR_ROIs(filename)
-filename = 'C:\Users\griff\Downloads\segmentation\10042_1_03OZYDY8\MR10.roi';
-d = importdata(filename);
-sz = size(d);
+function [mask,d,key] = Import_MR_ROIs(filename)
+% filename = 'C:\Users\griff\Downloads\segmentation\10042_1_03OZYDY8\MR10.roi';
+data = importdata(filename);
+sz = size(data);
 count = 1;  % Count number of datapoints per trace to create matrix
-old = 1;
+old = count;
 trace = 1;  % Count the number of traces
 key = [];
 form = '{\d*.\d*,\s\d*.\d*}††'; % Look for data that has the form: {'{180.55561828613281, 176.70474243164062}††'}
@@ -20,7 +21,7 @@ point = '[^0-9.]';
 
 %% Parse the .ROI data
 for i = 1:sz(1)
-    x = char(d(i,1));
+    x = char(data(i,1));
     match = regexp(x,form,'match');
     [~,m] = size(match);
     y = cell(3,m);
@@ -30,13 +31,44 @@ for i = 1:sz(1)
         y(2,j) = regexprep(z{:},point,'');
         z = regexp(match(1,j),yPoint,'match');
         y(3,j) = regexprep(z{:},point,'');
-        b(count,1) = str2double(y(2,j));
-        b(count,2) = str2double(y(3,j));
-        b(count,3) = trace;
+        a(count,1) = str2double(y(2,j));
+        a(count,2) = str2double(y(3,j));
+        a(count,3) = trace;
         count = count+1;
     end
     if count > old
         trace = trace+1;
     end
     old = count;
+end
+
+%% Interpolate and Create Masks
+d = [];
+f = [];
+aOriginal = a;
+res = 512; % Mask resolution
+for k = 1:trace-1
+    b = a(a(:,3) == k,:); % Work with one trace at a time
+    c = V_interpolate(b,5,5);
+    f = cat(1,f,c);
+    e = (c(:,1:2)-80).*5;
+    d = cat(1,d,e);
+    mask(:,:,k) = poly2mask(e(:,1),e(:,2),res,res);
+    Parameters  = regionprops(mask(:,:,k),'Centroid','Area','MajorAxisLength');
+    center(k,:) = Parameters.Centroid;
+    area(k) = Parameters.Area;
+    MAL(k) = Parameters.MajorAxisLength;
+end
+
+%% Find Labels for Each Trace
+d(:,3) = f(:,3);
+[n,~] = size(d);
+d = num2cell(d);
+idCode = FindDataLabels(filename);
+for i=1:n
+    j = cell2mat(d(i,3));
+    if j> size(idCode)
+        break;
+    end
+    d{i,4} = idCode{j};
 end
