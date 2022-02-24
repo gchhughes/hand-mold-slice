8cda0ac96bf2e6566414516fd9d14b92747b37ae
gchhughes <74072434+gchhughes@users.noreply.github.com>
2022-02-23 20:39:43 -0800

    Updated outputs

--- CompareROIs.m
+++ CompareROIs.m
@@ -18,16 +18,19 @@ addpath 'C:\Users\griff\github\hand-mold-slice'; % Path where functions are stor
 
 %% Import ROI Data
 fprintf('Importing...\n');
-s = ImportROIs(filename,path); % Change sheet name to "Ready" and move data there first
+s = ImportROIs(filename,path); % Change sheet name to "InProgress" and move data there first
+fprintf('Done\n');
 
 %% Alignment
 fprintf('Aligning...\n');
 s = RigidAlign(s);
 s = NonRigidAlign(s); % Requires Mapping, Statistics & Machine Learning Toolboxes
+fprintf('Done\n');
 
 %% Statistics
 fprintf('Computing Statistics...\n');
 s = Statistics(s,path);
+fprintf('Done\n');
 
 %% Store Data
 fprintf('Storing Data...\n');
